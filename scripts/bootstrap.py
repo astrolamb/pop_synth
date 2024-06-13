d66f333bf8dc9338d1524b4cc52ea4015dd452cd
William Lamb <60004701+astrolamb@users.noreply.github.com>
2024-06-13 14:45:36 +0100

    fixed bootstrapping

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 import numpy as np
 from scipy import stats
 
-def bootstrap(data):
+def bootstrap(n):
     """
     bootstrap the data
 
@@ -21,9 +21,8 @@ def bootstrap(data):
     data: array
         the bootstrapped data
     """
-    n = len(data)
-    idx = np.random.randint(0, n, n)
-    return data[idx]
+    idxs = np.random.randint(0, n, n)
+    return idxs
 
 def compute_stats(data):
     """
@@ -52,12 +51,15 @@ def compute_stats(data):
     kurt = stats.kurtosis(data, axis=0)
     return mu, var, skew, kurt
 
+
 if __name__ == '__main__':
 
     # parse the command line arguments
     parser = ArgumentParser()
     parser.add_argument('--model_idx', type=int,
                         help='the model index to bootstrap')
+    parser.add_argument('--real_idx', type=int,
+                        help='the idx of the realization')
     args = parser.parse_args()
 
     h2cf = np.load(f'./data/accre_runs/h2cf_model{args.model_idx}.npy')
@@ -65,15 +67,15 @@ if __name__ == '__main__':
     # bootstrap the data
     # save the statistics as a structured array with the same length as the
     # number of frequencies and number of bootstraps
-    stat_array = np.zeros((len(h2cf), 30), dtype=[('mean', float),
+    stat_array = np.zeros((100000, 30), dtype=[('mean', float),
                                                   ('variance', float),
                                                   ('skewness', float),
                                                   ('kurtosis', float)])
-    for ii in range(h2cf.shape[0]):
-        h2cf = bootstrap(h2cf)
+    for ii in range(100000):
+        h2cf_boot = h2cf[bootstrap(100000)]
 
         # compute the statistics
-        mean, variance, skewness, kurtosis = compute_stats(h2cf)
+        mean, variance, skewness, kurtosis = compute_stats(h2cf_boot)
 
         # store the statistics
         stat_array[ii]['mean'] = mean
@@ -83,8 +85,8 @@ if __name__ == '__main__':
 
         # save the statistics every 1000 iterations
         if ii % 1000 == 0:
-            np.save(f'./data/accre_runs/h2cf_model{args.model_idx}_stats.npy',
+            np.save(f'./data/accre_runs/h2cf_model{args.model_idx}_stats_{args.real_idx}.npy',
                     stat_array)
 
-    np.save(f'./data/accre_runs/h2cf_model{args.model_idx}_stats.npy',
+    np.save(f'./data/accre_runs/h2cf_model{args.model_idx}_stats_{args.real_idx}.npy',
             stat_array)
